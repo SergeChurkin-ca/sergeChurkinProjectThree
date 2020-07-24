12e223259b1d695774c6737d3692b7d3e44e7682
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-07-24 17:04:36 -0400

    reworked method of looping

--- script-2.js
+++ script-2.js
@@ -1,20 +1,67 @@
-function startQuiz(questions, countries, results, submitButton) {
+// Help please! I'm trying to build a quiz. It compares two nested objects: countries and questions.
 
+// I receive the value from questions, have to find the same value in countries and get the key which is the name of country.
 
-    function showQuestion(questions, countries) {
+// Countries and their parameters
+const app = {};
 
-    }
+app.init = function() {
 
-
-    function showResults(questions, countries, results) {
-
-    }
-
-
-    showQuestion(questions, countryCriterias);
-
-    $('button').on('click', function() {
-        showResults(questions, countries, results)
-        console.log('clicked');
-    })
 }
+
+app.countries = {
+
+    Norway: {
+        season: 'winter',
+        weather: ['stormy', 'snow-storms', 'snow'],
+        landscape: ['mountains', 'seaside'],
+        food: 'seafood',
+        music: ['rock', 'metal', 'electronic', 'techno'],
+        wayofLife: ['secluded', 'journeys and outdoors'],
+        dreamHouse: ['forest cabin', 'log house'],
+        annoyingPeople: ['talkative', 'bragging', 'lazy'],
+    },
+    Germany: {
+        season: ['winter', 'fall'],
+    },
+    Thailand: {
+        season: 'summer',
+        weather: ['sunny', 'hot'],
+        landscape: ['mountains', 'seaside', 'rainforest'],
+        food: ['seafood', 'thai'],
+        music: ['techno', 'k-pop'],
+        annoyingPeople: ['bragging'],
+    },
+}
+
+
+// Quiz Quesions
+app.questions = {
+
+    season: {
+        ask: "What are your favourie seasons?",
+        answer: ["winter ", "spring", "summer", "fall"],
+    },
+    weather: {
+        ask: "What are the most beautiful types weather?",
+        answer: ["sunny", "hot", "cold", "snow", ],
+    },
+    landscape: ["mountain", "rainforest", "Woodland ", "jungle", "seaside"],
+    food: ["indonesian", "turkish", "thai", "spanish", "morocan", "japanese", "indian", "italian", "french", "chinese", "american", "mediterranean", "seafoo"],
+    diet: ["vegetarian", "raw eater", "meat eater", "sweet tooth", "fitness"],
+    music: ["electronic dance music", "rock", "jazz", "classical", "rhythm and blues", "techno", "country", "pop music", "ethnic", "latin pop", "k-pop", "rap", "hip hop", "indie", "metal", "reggae", "soul"],
+}
+
+
+
+$('button').on('click', function(e) {
+    e.preventDefault();
+    const choice = [];
+    $.each($('input[name=answer]:checked'),
+        function() {
+            console.log(choice)
+            `enter code here`
+            choice.push($(this).val());
+            $('body').html(choice)
+        })
+})
--- script.js
+++ script.js
@@ -4,15 +4,16 @@
 const app = {};
 
 
+
 // building database
 app.countries = {
 
     Norway: {
-        season: 'winter',
+        score: 0,
+        season: ['Winter'],
         weather: ['stormy', 'snow-storms', 'snow'],
         landscape: ['mountains', 'seaside'],
-        weather: 'snow',
-        food: 'seafod',
+        food: 'seafood',
         music: ['rock', 'metal', 'electronic', 'techno'],
         wayofLife: ['secluded', 'journeys and outdoors'],
         dreamHouse: ['forest cabin', 'log house'],
@@ -21,66 +22,54 @@ app.countries = {
         movieRole: ['historical', 'cop', 'ordinary people'],
         loveIs: ['trust', 'strong family']
     },
-    Ukraine: {
-        season: ['winter', 'fall']
+    Germany: {
+        score: 0,
+        season: ['Winter', 'fall']
+    },
+    Thailand: {
+        score: 0,
+        season: 'summer',
+        weather: ['Sunny', 'hot'],
+        landscape: ['mountains', 'seaside'],
+        food: ['seafood', 'thai'],
+        music: ['techno', 'k-pop'],
+        annoyingPeople: ['bragging'],
+
     }
 
 }
 
+
+
 // quiz questions
-app.questions = {
+// app.questions = {
 
-    season: {
-        ask: "What are your favourie seasons?",
-        answer: ["Winter ", "Spring", "Summer", "Fall"],
+//     season: {
+//         ask: "What are your favourie seasons?",
+//         answer: ["Winter ", "Spring", "Summer", "Fall"],
 
-    },
-    weather: {
-        ask: "What are the most beautiful types weather?",
-        answer: ["Rainy", "Stormy", "Sunny", "Cloudy", "Hot", "Cold", "Dry", "Wet", "Windy", "Hurricanes", "typhoons", "Sand-storms", "Snow-storms", "Tornados", "Humid", "Foggy", "Snow", "Thundersnow", "Hail", "Sleet", "drought", "wildfire", "blizzard", "avalanche", "Mist", "Thunderstorms"],
-    },
-    landscape: ["Desert", "Plain", "Taiga", "Tundra", "Wetland ", "Mountain", "Mountain range", "Cliff", "Coast ", "Glacier", "Polar regions of Earth", "Shrubland", "Forest", "Rainforest", "Woodland ", "Jungle"],
-    food: ["indonesian", "turkish", "thai", "spanish", "morocan", "japanese", "indian", "italian", "french", "chinese", "american", "mediterranean"],
-    diet: ["vegetarian", "raw eater", "meat eater", "sweet tooth", "fitness"],
-    music: ["electronic dance music", "rock", "jazz", "classical", "rhythm and blues", "techno", "country", "pop music", "ethnic", "latin pop", "k-pop", "rap", "hip hop", "indie", "metal", "reggae", "soul"],
-    wayOfLife: ["Secluded", "Vibrant city life", "Adventurous and survival", "Rural, farming and gardening", "Journeys and lost of outdoor activities", "Venture"]
-}
+//     },
+//     weather: {
+//         ask: "What are the most beautiful types weather?",
+//         answer: ["Rainy", "Stormy", "Sunny", "Cloudy", "Hot", "Cold", "Dry", "Wet", "Windy", "Hurricanes", "typhoons", "Sand-storms", "Snow-storms", "Tornados", "Humid", "Foggy", "Snow", "Thundersnow", "Hail", "Sleet", "drought", "wildfire", "blizzard", "avalanche", "Mist", "Thunderstorms"],
+//     },
+//     landscape: ["Desert", "Plain", "Taiga", "Tundra", "Wetland ", "Mountain", "Mountain range", "Cliff", "Coast ", "Glacier", "Polar regions of Earth", "Shrubland", "Forest", "Rainforest", "Woodland ", "Jungle"],
+//     food: ["indonesian", "turkish", "thai", "spanish", "morocan", "japanese", "indian", "italian", "french", "chinese", "american", "mediterranean", "seafoo"],
+//     diet: ["vegetarian", "raw eater", "meat eater", "sweet tooth", "fitness"],
+//     music: ["electronic dance music", "rock", "jazz", "classical", "rhythm and blues", "techno", "country", "pop music", "ethnic", "latin pop", "k-pop", "rap", "hip hop", "indie", "metal", "reggae", "soul"],
+//     wayOfLife: ["Secluded", "Vibrant city life", "Adventurous and survival", "Rural, farming and gardening", "Journeys and lost of outdoor activities", "Venture"]
+// }
 
-const countryCriterias = Object.values(app.countries);
-
-console.log(countryCriterias);
-
-
+// const countryCriterias = Object.values(app.countries);
+// console.log(countryCriterias);
 // const questionParameters = Object.values(app.questions);
-
-for (index in app.questions) {
-    console.log("this is " +
-        app.questions[index]);
-}
-
-
-
 // const listOfCountries = Object.keys(app.countries);
 // console.log(listOfCountries)
-
-
-
-
-
 // If statements ckecks if the sellected answer coincide with feature objects element of country
 // After input another question is rendered on page
-
 // submit button confirms the choice and starts next question
-
-
 // Math.random finalize the result to exclude the coincidence of countries with similar features
-
-
-
 // Backgdound image of container provides picture from unsplash.com or pixabay.com and illustrates the choice and some informaiton about the country
-
-
-
 
 
 //1. Output array of questions on html in ul>li
@@ -88,26 +77,75 @@ for (index in app.questions) {
 
 // ------------------------------------------
 // Displaying quiz quesionts dynamically
+// get values of selection on submit click
 // ------------------------------------------
-for (let index in app.questions) {
-    for (let i in app.questions[index].answer) {
-        $('.question').append(`
-    <ul>
-    ${app.questions[index].ask}
-        <li>
-        <input type="checkbox" name="answer" id="answer" value="answer">
-        ${app.questions[index].answer[i]}
-        </li>
-        
-    </ul>
-    `);
+
+
+// for (let index in app.questions) {
+//     for (let i in app.questions[index].answer) {
+
+
+//         $('.question').append(`
+//     <ul>
+//     ${app.questions[index].ask}
+//         <li>
+//         <input type="checkbox" name="answer" id="answer" value="${app.questions[index].answer[i]}">
+//         ${app.questions[index].answer[i]}   
+//         </li>    
+//     </ul>
+//     `);
+
+
+
+//     }
+$('button').on('click', function(e) {
+    e.preventDefault();
+    const choice = [];
+
+
+    let countryScore = 0;
+    $.each($('input:checked'),
+            function() {
+                console.log(choice)
+
+                // console.log($(this).attr('name'));
+                // choice.push($(this).val());
+                const userChoice = {
+                        category: $(this).attr('name'),
+                        selection: $(this).val()
+
+                    }
+                    // console.log(userChoice);
+                    // $('body').html(choice)
+                    // countryScore++;
+                    // console.log("score is " + countryScore)
+
+                choice.push(userChoice);
+
+            })
+        // this is the selection after checkbox checked
+
+    serchForAnswer(choice);
+    //find the list of countrie
+})
+
+const serchForAnswer = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let countryName in app.countries) {
+            console.log(app.countries[countryName][arr[i].category]);
+            console.log(arr[i])
+            if (app.countries[countryName][arr[i].category].includes(arr[i].selection)) {
+                console.log(countryName)
+            }
+
+        }
+
     }
-    $('button').on('click', function(e) {
-        e.preventDefault();
-
-        const value = $(this)('input[name="answer]:checked').val();
-        console.log(value)
-
-    })
 }
-// ${app.questions[index].answer[i]}
+
+
+// }
+
+// $(function() {
+//     app.init();
+// })
